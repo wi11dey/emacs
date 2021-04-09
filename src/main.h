6e0f07448bbdeba6bb413afed51cbcd47aba3af2
Will Dey <52264243+wi11dey@users.noreply.github.com>
2021-04-09 11:10:29 -0500

    apply uemacs patchset

---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define PROGRAM_NAME "em"
+#define PROGRAM_NAME_LONG "uEmacs/Pk"
+
+#define	VERSION	"4.0.15"
+
+int main(int argc, char **argv);
+
+#endif  /* MAIN_H_ */
